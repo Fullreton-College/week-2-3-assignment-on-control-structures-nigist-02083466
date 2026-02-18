7a8388c4c155b092ad9dad6de37c6261503cf236
Tekle, Nigist <nigist.tekle21@gmail.com>
2026-02-18 15:02:50 -0800

    code changes complet

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
+    double lengthA, widthA;
+    double lengthB, widthB;
+     
+    cout<<"Enter length and width of first rectange A: ";
+    cin>>lengthA>>widthB;
+
+    cout<<"Enter length and width of second rectangle B ";
+    cin>>lengthA>>widthB;
+
+      double AreaA = lengthA * widthB;
+      double AreaB = lengthB * widthB;
+
+    if( AreaA > AreaB )
+    {
+        cout<<"first rectangle A has a larger Area"<<endl;
+    }
+    else if ( AreaA < AreaB)
+    {
+        cout<<"second rectangle B have the same Area"<<endl;
+    }
 	
     return 0;
 }
